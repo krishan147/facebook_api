62702879236081f6dbf51344d081097fe5630fe3
krishan vadher <krishan.vadher@gmail.com>
2020-01-03 10:17:12 +0000

    latest push

--- merger.py
+++ merger.py
@@ -7,4 +7,5 @@ def merge():
     all_filenames = [i for i in glob.glob('*.{}'.format(extension))]
     combined_csv = pd.concat([pd.read_csv(f) for f in all_filenames])
     combined_csv.to_csv("shavecare_combined.csv", index=False, encoding='utf-8-sig')
+    
 merge()
